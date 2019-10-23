95bdbe39bb2bf9c0d08fd73fb02b1f6d595f3955
vagrant <vagrant@vagrant.vm>
2019-10-23 09:30:31 +0200

    added routing modules

--- exo1/src/app/main/main.module.ts
+++ exo1/src/app/main/main.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
+import { HomeComponent } from './home/home.component';
+import { LegalComponent } from './legal/legal.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent, LegalComponent],
   imports: [
     CommonModule,
     MainRoutingModule
