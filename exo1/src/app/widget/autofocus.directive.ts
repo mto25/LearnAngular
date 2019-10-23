547c4d4e4093f2c1f20e5d560b4d4f54804bde8f
vagrant <vagrant@vagrant.vm>
2019-10-23 16:34:49 +0200

    commit temp

--- exo1/src/app/widget/autofocus.directive.ts
+++ exo1/src/app/widget/autofocus.directive.ts
@@ -1,12 +1,16 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appAutofocus]'
 })
-export class AutofocusDirective {
+export class AutofocusDirective implements OnInit {
 
-  constructor() {
-    console.log("test autofocus directive");
-   }
-
+  constructor(private elt: ElementRef) {
+    //console.log("test autofocus directive");
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.elt.nativeElement.focus();
+  }
 }
