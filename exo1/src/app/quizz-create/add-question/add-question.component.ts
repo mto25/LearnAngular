35a236342526cf2f1aa8eef6e4ae5e46938eda96
vagrant <vagrant@vagrant.vm>
2019-10-24 11:45:36 +0200

    ta index

--- exo1/src/app/quizz-create/add-question/add-question.component.ts
+++ exo1/src/app/quizz-create/add-question/add-question.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-question.component.scss']
 })
 export class AddQuestionComponent implements OnInit {
+
+  f = new FormGroup({
+    label: new FormControl('quelles est la capitale de la Belgique?', Validators.required),
+    answerA: new FormControl('Brux', Validators.required),
+    answerB: new FormControl('Pris', Validators.required),
+    answerC: new FormControl('Rome', Validators.required),
+    answerD: new FormControl('Buch', Validators.required),
+    correctAnswer : new FormControl('', Validators.required)
+  });
 
   constructor() { }
 
